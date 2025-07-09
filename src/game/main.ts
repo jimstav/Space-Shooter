36e9cbaf22d9e639ba7bdebbc9b39e1ea0e6e634
Dimitris Stavroulakis <jimstav@gmail.com>
2025-07-10 00:25:47 +0300

    Refactor to match setup files state

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,19 +1,28 @@
-import { Boot } from "./scenes/Boot";
-import { GameOver } from "./scenes/GameOver";
+import { Game } from "phaser";
 import { Game as MainGame } from "./scenes/Game";
-import { MainMenu } from "./scenes/MainMenu";
-import { AUTO, Game } from "phaser";
-import { Preloader } from "./scenes/Preloader";
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
-  type: AUTO,
-  width: 1024,
-  height: 768,
-  parent: "game-container",
-  backgroundColor: "#028af8",
-  scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+  type: Phaser.CANVAS,
+  roundPixels: true,
+  pixelArt: true,
+  scale: {
+    parent: "game-container",
+    width: 450,
+    height: 640,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH,
+  },
+  backgroundColor: "#000000",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0, x: 0 },
+      debug: true,
+    },
+  },
+  scene: [MainGame],
 };
 
 const StartGame = (parent: string) => {
--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -2,43 +2,21 @@ import { EventBus } from "../EventBus";
 import { Scene } from "phaser";
 
 export class Game extends Scene {
-  camera: Phaser.Cameras.Scene2D.Camera;
-  background: Phaser.GameObjects.Image;
-  gameText: Phaser.GameObjects.Text;
-
   constructor() {
     super("Game");
   }
 
   create() {
-    this.camera = this.cameras.main;
-    this.camera.setBackgroundColor(0x00ff00);
-
-    this.background = this.add.image(512, 384, "background");
-    this.background.setAlpha(0.5);
-
-    this.gameText = this.add
-      .text(
-        512,
-        384,
-        "Make something fun!\nand share it with us:\nsupport@phaser.io",
-        {
-          fontFamily: "Arial Black",
-          fontSize: 38,
-          color: "#ffffff",
-          stroke: "#000000",
-          strokeThickness: 8,
-          align: "center",
-        }
-      )
-      .setOrigin(0.5)
-      .setDepth(100);
-
+    this.add
+      .text(this.scale.width / 2, this.scale.height / 2, "Hello World", {
+        fontSize: "32px",
+      })
+      .setOrigin(0.5);
     EventBus.emit("current-scene-ready", this);
   }
 
   changeScene() {
-    this.scene.start("GameOver");
+    this.scene.start("Game");
   }
 }
 
