679a82c4a2d9dcfc00acdd8d2f44cf1a1d119385
Dimitris Stavroulakis <jimstav@gmail.com>
2025-08-02 19:48:45 +0300

    Add backgrounds

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -17,6 +17,27 @@ export class Game extends Scene {
   }
 
   create() {
+    this.add
+      .sprite(0, 0, "bg1", 0)
+      .setOrigin(0, 1)
+      .setAlpha(0.7)
+      .setAngle(90)
+      .setScale(1, 1.25)
+      .play("bg1");
+    this.add
+      .sprite(0, 0, "bg2", 0)
+      .setOrigin(0, 1)
+      .setAlpha(0.7)
+      .setAngle(90)
+      .setScale(1, 1.25)
+      .play("bg2");
+    this.add
+      .sprite(0, 0, "bg3", 0)
+      .setOrigin(0, 1)
+      .setAlpha(0.7)
+      .setAngle(90)
+      .setScale(1, 1.25)
+      .play("bg3");
     const eventBusComponent = new EventBusComponent();
     const player = new Player(this);
     const scoutSpawner = new EnemySpawnerComponent(
