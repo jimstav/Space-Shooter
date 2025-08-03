c35548959487006e2339bb1699951bacb55efd2c
Dimitris Stavroulakis <jimstav@gmail.com>
2025-08-04 00:03:24 +0300

    Additional audio

--- src/objects/AudioManager.ts
+++ src/objects/AudioManager.ts
@@ -11,6 +11,8 @@ export class AudioManager {
     this.#scene = scene;
     this.#eventBusComponent = eventBusComponent;
 
+    this.#scene.sound.play("bg", { volume: 0.6, loop: true });
+
     this.#eventBusComponent.on(CUSTOM_EVENTS.ENEMY_DESTROYED, () => {
       this.#scene.sound.play("explosion", {
         volume: 0.6,
@@ -20,6 +22,18 @@ export class AudioManager {
     this.#eventBusComponent.on(CUSTOM_EVENTS.PLAYER_DESTROYED, () => {
       this.#scene.sound.play("explosion", {
         volume: 0.6,
+      });
+    });
+
+    this.#eventBusComponent.on(CUSTOM_EVENTS.SHIP_HIT, () => {
+      this.#scene.sound.play("hit", {
+        volume: 0.6,
+      });
+    });
+
+    this.#eventBusComponent.on(CUSTOM_EVENTS.SHIP_SHOOT, () => {
+      this.#scene.sound.play("shot1", {
+        volume: 0.05,
       });
     });
   }
